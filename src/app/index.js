d7fade2c1b08709c1124ce6022d7f06dfb4de656
Nasimjibs <nasimjibs@gmail.com>
2017-04-17 17:52:21 +0200

    Changed components

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-class App extends React.Component {
+const API_KEY = 'AIzaSyAX4iDX0i3CnRO0Hc6Rd9jmtFwiQd3xQgw';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos:[],
+      selectedVideo : null
+    };
+    YTSearch({key:API_KEY, term:'Cricket'},(videos)=>{
+      this.setState({
+        videos:videos,
+        selectedVideo : videos[0]
+      })
+    });
+  }
+
   render(){
     return(
       <div className="container">
-          <div className="row">
-              <div className="col-xs-10 col-xs-offset-1">
-                <p>It Works fine!</p>
-              </div>
-          </div>
+          <SearchBar/>
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList
+            onVideoSelect = {(selectedVideo)=> {this.setState(selectedVideo:selectedVideo)}}
+            videos={this.state.videos}/>
       </div>
     );
   }
